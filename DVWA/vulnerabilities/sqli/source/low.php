ac0258b1d3ad3ad932210eba93560528ae92e901
esik11 <160854319+esik11@users.noreply.github.com>
2025-05-21 20:15:35 +0800

    modified for preventing sql injection
    can comment/uncomment it for demonstration

--- DVWA/vulnerabilities/sqli/source/low.php
+++ DVWA/vulnerabilities/sqli/source/low.php
@@ -3,9 +3,16 @@
 if( isset( $_REQUEST[ 'Submit' ] ) ) {
 	// Get input
 	$id = $_REQUEST[ 'id' ];
+	
+	// SECURE VERSION: Input validation
+	if (!is_numeric($id)) {
+		die("Invalid input - ID must be a number");
+	}
 
 	switch ($_DVWA['SQLI_DB']) {
 		case MYSQL:
+			// VULNERABLE VERSION (commented out)
+			/*
 			// Check database
 			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
 			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
@@ -19,12 +26,38 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 				// Feedback for end user
 				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
 			}
+			*/
 
+			// SECURE VERSION: Using prepared statement
+			if ($stmt = mysqli_prepare($GLOBALS["___mysqli_ston"], "SELECT first_name, last_name FROM users WHERE user_id = ?")) {
+				// Bind parameters
+				mysqli_stmt_bind_param($stmt, "i", $id);
+				
+				// Execute query
+				mysqli_stmt_execute($stmt);
+				
+				// Bind result variables
+				mysqli_stmt_bind_result($stmt, $first, $last);
+				
+				// Fetch results and display securely
+				while (mysqli_stmt_fetch($stmt)) {
+					// Use htmlspecialchars to prevent XSS
+					$html .= "<pre>ID: " . htmlspecialchars($id) . 
+							"<br />First name: " . htmlspecialchars($first) . 
+							"<br />Surname: " . htmlspecialchars($last) . "</pre>";
+				}
+				
+				// Close statement
+				mysqli_stmt_close($stmt);
+			}
+			
 			mysqli_close($GLOBALS["___mysqli_ston"]);
 			break;
 		case SQLITE:
 			global $sqlite_db_connection;
-
+			
+			// VULNERABLE VERSION (commented out)
+			/*
 			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
 			#$sqlite_db_connection->enableExceptions(true);
 
@@ -48,6 +81,29 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 				}
 			} else {
 				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
+			}
+			*/
+
+			// SECURE VERSION: Using prepared statement for SQLite
+			try {
+				$stmt = $sqlite_db_connection->prepare("SELECT first_name, last_name FROM users WHERE user_id = :id");
+				$stmt->bindParam(':id', $id, SQLITE3_INTEGER);
+				$results = $stmt->execute();
+				
+				if ($results) {
+					while ($row = $results->fetchArray()) {
+						// Get values and escape output
+						$first = htmlspecialchars($row["first_name"]);
+						$last  = htmlspecialchars($row["last_name"]);
+						
+						// Feedback for end user
+						$html .= "<pre>ID: " . htmlspecialchars($id) . 
+								"<br />First name: {$first}<br />Surname: {$last}</pre>";
+					}
+				}
+			} catch (Exception $e) {
+				echo 'Caught exception: ' . htmlspecialchars($e->getMessage());
+				exit();
 			}
 			break;
 	} 
